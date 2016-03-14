5b7ce009e0c76f1fa818397a43c7b0b80a944525
Micheál Looney <hello@micheallooney.com>
2016-03-14 11:47:23 +0000

    Initial Commit

---
+++ myapp/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+
+    url(r'^signup/$', views.signupView, name='signupView'),
+    url(r'^signupHandler/$', views.signupHandlerView, name='signupHandlerView'),
+    url(r'^welcome/$', views.signupLandingView, name='signupLandingView'),
+
+    url(r'^login/', views.loginView, name='loginView'),
+    url(r'^auth/$', views.authHandlerView, name='authHandlerView'),
+
+    url(r'^logout/$', views.logoutView, name='logoutView'),
+    url(r'^loggedin/$', views.loggedinView, name='loggedinView'),
+    url(r'^invalid/$', views.invalidView, name='invalidView'),
+
+    url(r'^secure/$', views.securedView, name='securedView'),
+
+]
+
+
+
---
+++ myapp/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import auth
+from django.core.context_processors import csrf
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+def index(request):
+    return HttpResponse("<h2>HEY!</h2>")
+
+def loginView(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('myapp/login.html', c)
+
+def authHandlerView(request):
+    username  = request.POST.get('username', '')
+    password  = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/myapp/loggedin')
+    else:
+        return HttpResponseRedirect('/myapp/invalid')
+
+def logoutView(request):
+    pass
+
+@login_required
+def securedView(request):
+    return HttpResponse("<h2>Secured urL</h2>")
+
+def loggedinView(request):
+    return HttpResponse("<h2>Logged in</h2>")
+
+def invalidView(request):
+    return HttpResponse("<h2>Invalid Credentials</h2>")
+
+def signupView(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('myapp/signup.html', c)
+
+def signupHandlerView(request):
+    username  = request.POST.get('username', '')
+    email  = request.POST.get('emali', '')
+    password  = request.POST.get('password', '')
+    user = User.objects.create_user(username, email , password)
+    return HttpResponseRedirect('/myapp/welcome')
+
+def signupLandingView(request):
+    return HttpResponse("<h2>New user created</h2><h2><a href='/myapp/secure/'>secure page</a></h2>")
+
+
+
